9fd7b35881a96c2a51a2fd45e937362d06480237
Colin Hicks <colin.hicks@gmail.com>
2016-09-24 13:28:31 -0400

    release prep

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject net.colinhicks/galvani "0.0.1-SNAPSHOT"
+  :description "A Clojure client for reading DynamoDB Streams"
+  :url "https://github.com/colinhicks/galvani"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.amazonaws/aws-java-sdk-dynamodb "1.11.24"]
                  [org.clojure/core.async "0.2.391"]
